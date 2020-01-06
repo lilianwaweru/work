757194d1302cb4d121fd96e0d0ca546b40b3abf9
lilianwaweru <lilowesh.lw@gmail.com>
2020-01-06 11:12:53 +0300

    added a jumborton

--- new/urls.py
+++ new/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
+    url(r'^report/',views.report,name = 'report'),
     url(r'^$',views.welcome,name = 'welcome'),
-    url(r'^report/',views.report,name = 'report')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
